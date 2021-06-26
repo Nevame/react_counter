9245bf7ff30a0618a15fb0ec0bf286534101c3a0
Simony <anyomisimon@gmail.com>
2021-06-26 12:07:18 +0000

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassCounter from './component/ClassCounter';
+import FunctionCounter from './component/FunctionCounter';
 
 function App() {
   return (
     <>
     <ClassCounter />
+    <FunctionCounter />
     </>
   );
 }
